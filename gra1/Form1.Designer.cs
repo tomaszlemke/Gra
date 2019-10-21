f4724f917a6eb0ef85bdf731480838bd3692baa1
tomaszlemke <47919500+tomaszlemke@users.noreply.github.com>
2019-10-21 12:05:19 +0200

    Add files via upload

---
+++ gra1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace gra1
+{
+    partial class gra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlPanel = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tmrStoper = new System.Windows.Forms.Timer(this.components);
+            this.tmrPomiarCzasuRozgrywki = new System.Windows.Forms.Timer(this.components);
+            this.lblCzas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnlPanel
+            // 
+            this.pnlPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pnlPanel.Location = new System.Drawing.Point(18, 18);
+            this.pnlPanel.Name = "pnlPanel";
+            this.pnlPanel.Size = new System.Drawing.Size(390, 371);
+            this.pnlPanel.TabIndex = 0;
+            this.pnlPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlPanel_Paint);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(18, 426);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(127, 39);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // tmrStoper
+            // 
+            this.tmrStoper.Interval = 1000;
+            this.tmrStoper.Tick += new System.EventHandler(this.TmrStoper_Tick);
+            // 
+            // tmrPomiarCzasuRozgrywki
+            // 
+            this.tmrPomiarCzasuRozgrywki.Interval = 25;
+            this.tmrPomiarCzasuRozgrywki.Tick += new System.EventHandler(this.TmrPomiarCzasuRozgrywki_Tick);
+            // 
+            // lblCzas
+            // 
+            this.lblCzas.AutoSize = true;
+            this.lblCzas.Location = new System.Drawing.Point(185, 439);
+            this.lblCzas.Name = "lblCzas";
+            this.lblCzas.Size = new System.Drawing.Size(80, 13);
+            this.lblCzas.TabIndex = 2;
+            this.lblCzas.Text = "Czas rozgrywki:";
+            // 
+            // gra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 477);
+            this.Controls.Add(this.lblCzas);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlPanel);
+            this.Name = "gra";
+            this.Text = "gra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Gra_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPanel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer tmrStoper;
+        private System.Windows.Forms.Timer tmrPomiarCzasuRozgrywki;
+        private System.Windows.Forms.Label lblCzas;
+    }
+}
+
---
+++ gra1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace gra1
+{
+    public partial class gra : Form
+    {
+        Random losowe = new Random();
+        Control.ControlCollection pola;
+        bool czyKolej;
+        Color KolorPrzeszkody = Color.DarkViolet, colorSterownika = Color.Crimson;
+        int pozycja, ominieteElementy;
+
+        private Stopwatch watch = new Stopwatch();
+
+        private void UpdateTime()
+        {
+
+            lblCzas.Text = "Czas rozgrywki: " + GetTimeString(watch.Elapsed);
+
+        }
+
+        private string GetTimeString(TimeSpan elapsed)
+        {
+            string result = string.Empty;
+
+            result = string.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                elapsed.Hours,
+                elapsed.Minutes,
+                elapsed.Seconds,
+                elapsed.Milliseconds);
+
+            return result;
+        }
+
+        public gra()
+        {
+            InitializeComponent();
+            UpdateTime();
+        }
+
+        private void PnlPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j< 11; j++)
+
+            for (int i = 0; i < 5; i++)
+            {
+                TextBox pole = new TextBox();
+                pole.Width = 70;
+                pole.Left = i * 75 + 10;
+                pole.Top = j * 30 +10;
+                pole.Enabled = false;
+                this.pnlPanel.Controls.Add(pole);
+            }
+        }
+
+        private void TmrStoper_Tick(object sender, EventArgs e)
+        {
+            
+            for (int i = 54; i > 4; i--)
+                pola[i].BackColor = pola[i - 5].BackColor;
+            
+            for (int i = 0; i < 5; i++)
+                pola[i].BackColor = SystemColors.Window;
+
+            czyKolej = !czyKolej;
+
+            if (czyKolej)
+                for (int i = 0; i < 4; i++)
+                    pola[losowe.Next(0, 5)].BackColor = KolorPrzeszkody;
+                    
+            tmrStoper.Interval = (int)(tmrStoper.Interval * 0.995);
+
+            for (int i = 50; i < 55; i++)
+                if (pola[i].BackColor == KolorPrzeszkody)
+                    ominieteElementy++;
+            
+            if (pola[pozycja +50].BackColor == KolorPrzeszkody)
+            {
+                
+                tmrStoper.Stop();
+                tmrPomiarCzasuRozgrywki.Stop();
+                pola[50 + pozycja].BackColor = Color.Black;
+                MessageBox.Show("Game over. Liczba ominietych elementów: " + (ominieteElementy -1));
+                btnStart.Enabled = true;
+                foreach (Control pole in pnlPanel.Controls)
+                   pole.BackColor = SystemColors.Window;
+                watch.Stop();
+            }
+
+            else
+                pola[50 + pozycja].BackColor = colorSterownika;
+        }
+
+        private void TmrPomiarCzasuRozgrywki_Tick(object sender, EventArgs e)
+        {
+            lblCzas.Text = "Czas rozgrywki: " + GetTimeString(watch.Elapsed);
+        }
+
+        private void Gra_KeyDown(object sender, KeyEventArgs e)
+        {
+            pola[50 + pozycja].BackColor = SystemColors.Window;
+
+            
+            if (e.KeyValue == 37 && pozycja > 0 && pola[pozycja +49].BackColor ==SystemColors.Window)
+                pozycja--;
+                
+
+            if (e.KeyValue == 39 && pozycja < 4 && pola[pozycja + 51].BackColor == SystemColors.Window)
+                pozycja++;
+                
+            pola[50 + pozycja].BackColor = colorSterownika;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            pola = this.pnlPanel.Controls;
+            tmrStoper.Enabled = true;
+            pozycja = 2;
+            ominieteElementy = 0;
+            tmrStoper.Interval = 1000;
+            tmrPomiarCzasuRozgrywki.Enabled = true;
+            btnStart.Enabled = false;
+            pola[50 + pozycja].BackColor = colorSterownika;
+            watch.Start();
+        }
+
+    }
+}
